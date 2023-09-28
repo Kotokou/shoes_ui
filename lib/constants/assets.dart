b64f0cbded2b01e1d7aff72fee69e49cc20d4c3d
KOTOKOU Komla David <kotokou74@gmail.com>
2023-09-28 21:10:45 +0000

    setup

---
+++ lib/constants/assets.dart
@@ -0,0 +1,17 @@
+class Assets {
+  Assets._();
+
+  static const String bag = "assets/images/bag.svg";
+  static const String basket1 = "assets/images/basket1.png";
+  static const String basket2 = "assets/images/basket2.png";
+  static const String basket3 = "assets/images/basket3.png";
+  static const String basket4 = "assets/images/basket4.png";
+  static const String basket5 = "assets/images/basket5.png";
+  static const String box = "assets/images/box.png";
+  static const String collection = "assets/images/collection.svg";
+  static const String home = "assets/images/home.svg";
+  static const String menu = "assets/images/menu.svg";
+  static const String nike = "assets/images/nike.svg";
+  static const String notification = "assets/images/notification.svg";
+  static const String person = "assets/images/person.svg";
+}
---
+++ lib/models/product_model.dart
@@ -0,0 +1,11 @@
+class ProductModel {
+  final String image;
+  final String label;
+  final String price;
+
+  ProductModel({
+    required this.image,
+    required this.label,
+    required this.price,
+  });
+}
